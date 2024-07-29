2fa80ffcb0c58aeb4e268ab2878308d7dcc00127
insu <qkrdlstn9701@naver.com>
2024-07-29 20:48:26 +0900

    update ch04 not final

--- DeepLearning02/function.py
+++ DeepLearning02/function.py
@@ -3,6 +3,7 @@ import streamlit as st
 from stqdm import stqdm
 import time
 import matplotlib.pyplot as plt
+import collections
 
 def preprocess(text):
     text = text.lower()
@@ -327,13 +328,13 @@ class Trainer:
         loss_count = 0
 
         start_time = time.time()
-        for epoch in range(max_epoch):
+        for epoch in stqdm(range(max_epoch)):
             # 뒤섞기
             idx = np.random.permutation(np.arange(data_size))
             x = x[idx]
             t = t[idx]
 
-            for iters in range(max_iters):
+            for iters in stqdm(range(max_iters)):
                 batch_x = x[iters*batch_size:(iters+1)*batch_size]
                 batch_t = t[iters*batch_size:(iters+1)*batch_size]
 
@@ -367,3 +368,222 @@ class Trainer:
         plt.xlabel('iterations (x' + str(self.eval_interval) + ')')
         plt.ylabel('loss')
         st.pyplot(plt)
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        np.add.at(dW, self.idx, dout)
+        return None
+    
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+
+        self.cache = (h, target_W)
+        return out
+
+    def backward(self, dout):
+        h, target_W = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+
+        dtarget_W = dout * h
+        self.embed.backward(dtarget_W)
+        dh = dout * target_W
+        return dh
+    
+class UnigramSampler:
+    def __init__(self, corpus, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = None
+        self.word_p = None
+
+        counts = collections.Counter()
+        for word_id in corpus:
+            counts[word_id] += 1
+
+        vocab_size = len(counts)
+        self.vocab_size = vocab_size
+
+        self.word_p = np.zeros(vocab_size)
+        for i in range(vocab_size):
+            self.word_p[i] = counts[i]
+
+        self.word_p = np.power(self.word_p, power)
+        self.word_p /= np.sum(self.word_p)
+
+    def get_negative_sample(self, target):
+        batch_size = target.shape[0]
+
+        GPU = False
+
+        if not GPU:
+            negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
+
+            for i in range(batch_size):
+                p = self.word_p.copy()
+                target_idx = target[i]
+                p[target_idx] = 0
+                p /= p.sum()
+                negative_sample[i, :] = np.random.choice(self.vocab_size, size=self.sample_size, replace=False, p=p)
+        else:
+            # GPU(cupy）로 계산할 때는 속도를 우선한다.
+            # 부정적 예에 타깃이 포함될 수 있다.
+            negative_sample = np.random.choice(self.vocab_size, size=(batch_size, self.sample_size),
+                                               replace=True, p=self.word_p)
+
+        return negative_sample
+
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None  # sigmoid의 출력
+        self.t = None  # 정답 데이터
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+        return dx
+
+class NegativeSamplingLoss:
+    def __init__(self, W, corpus, power=0.75, sample_size=5):
+        self.sample_size = sample_size
+        self.sampler = UnigramSampler(corpus, power, sample_size)
+        self.loss_layers = [SigmoidWithLoss() for _ in range(sample_size + 1)]
+        self.embed_dot_layers = [EmbeddingDot(W) for _ in range(sample_size + 1)]
+
+        self.params, self.grads = [], []
+        for layer in self.embed_dot_layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, h, target):
+        batch_size = target.shape[0]
+        negative_sample = self.sampler.get_negative_sample(target)
+
+        # 긍정적 예 순전파
+        score = self.embed_dot_layers[0].forward(h, target)
+        correct_label = np.ones(batch_size, dtype=np.int32)
+        loss = self.loss_layers[0].forward(score, correct_label)
+
+        # 부정적 예 순전파
+        negative_label = np.zeros(batch_size, dtype=np.int32)
+        for i in range(self.sample_size):
+            negative_target = negative_sample[:, i]
+            score = self.embed_dot_layers[1 + i].forward(h, negative_target)
+            loss += self.loss_layers[1 + i].forward(score, negative_label)
+
+        return loss
+
+    def backward(self, dout=1):
+        dh = 0
+        for l0, l1 in zip(self.loss_layers, self.embed_dot_layers):
+            dscore = l0.backward(dout)
+            dh += l1.backward(dscore)
+
+        return dh
+
+class CBOW:
+    def __init__(self, vocab_size, hidden_size, window_size, corpus):
+        V, H = vocab_size, hidden_size
+
+        # 가중치 초기화
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(V, H).astype('f')
+
+        # 계층 생성
+        self.in_layers = []
+        for i in range(2 * window_size):
+            layer = Embedding(W_in)  # Embedding 계층 사용
+            self.in_layers.append(layer)
+        self.ns_loss = NegativeSamplingLoss(W_out, corpus, power=0.75, sample_size=5)
+
+        # 모든 가중치와 기울기를 배열에 모은다.
+        layers = self.in_layers + [self.ns_loss]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # 인스턴스 변수에 단어의 분산 표현을 저장한다.
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):
+        h = 0
+        for i, layer in enumerate(self.in_layers):
+            h += layer.forward(contexts[:, i])
+        h *= 1 / len(self.in_layers)
+        loss = self.ns_loss.forward(h, target)
+        return loss
+
+    def backward(self, dout=1):
+        dout = self.ns_loss.backward(dout)
+        dout *= 1 / len(self.in_layers)
+        for layer in self.in_layers:
+            layer.backward(dout)
+        return None
+    
+def analogy(a, b, c, word_to_id, id_to_word, word_matrix, top=5, answer=None):
+    for word in (a, b, c):
+        if word not in word_to_id:
+            st.write('%s(을)를 찾을 수 없습니다.' % word)
+            return
+
+    st.write('\n[analogy] ' , a , ':' , b , ' = ' , c , ':?')
+    a_vec, b_vec, c_vec = word_matrix[word_to_id[a]], word_matrix[word_to_id[b]], word_matrix[word_to_id[c]]
+    query_vec = b_vec - a_vec + c_vec
+    query_vec = normalize(query_vec)
+
+    similarity = np.dot(word_matrix, query_vec)
+
+    if answer is not None:
+        st.write("==>" + answer + ":" + str(np.dot(word_matrix[word_to_id[answer]], query_vec)))
+
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if np.isnan(similarity[i]):
+            continue
+        if id_to_word[i] in (a, b, c):
+            continue
+        st.write(' {0}: {1}'.format(id_to_word[i], similarity[i]))
+
+        count += 1
+        if count >= top:
+            return
+        
+def normalize(x):
+    if x.ndim == 2:
+        s = np.sqrt((x * x).sum(1))
+        x /= s.reshape((s.shape[0], 1))
+    elif x.ndim == 1:
+        s = np.sqrt((x * x).sum())
+        x /= s
+    return x
--- DeepLearning02/main.py
+++ DeepLearning02/main.py
@@ -3,11 +3,12 @@ from function import *
 import numpy as np
 import matplotlib.pyplot as plt
 import ptb
+import pickle
 
 st.set_page_config(layout="wide")
 st.title("Deep-Learning-from-Scratch02")
 
-ch02, ch03 = st.tabs(['ch02', 'ch03'])
+ch02, ch03 , ch04= st.tabs(['ch02', 'ch03', 'ch04'])
 
 with ch02:
 
@@ -174,3 +175,106 @@ with ch03:
         # word_vecs = model.word_vecs
         # for word_id, word in id_to_word.items():
         #     st.write(word, word_vecs[word_id])
+
+with ch04:
+    col1, col2= st.columns([2,1])
+
+    with col1:
+        text = st.text_input("text_3", "You say goodbye and I say hello.")
+
+        st.write('---')
+
+        corpus, word_to_id, id_to_word = preprocess(text)
+        contexts, target = create_contexts_target(corpus, window_size=1)
+
+        col1_1, col1_2, col1_3 = st.columns([1,1,1])
+
+        with col1_1:
+            st.write("corpus : ", corpus)
+
+        with col1_2:
+            power = 0.75
+            sample_size = 2
+
+            st.write("power : ", power)
+            st.write("sample_size : ", sample_size)
+            target = np.array([1,3,0])
+            st.write("target : ", target)
+        
+        with col1_3:
+            st.markdown("### negative sample")
+            sampler = UnigramSampler(corpus, power, sample_size)
+            negative_sample = sampler.get_negative_sample(target)
+
+            st.write(negative_sample)
+
+        st.write("---")
+    
+    with col2:
+        st.markdown("## PTB DataSet")
+        # # 하이퍼파라미터 설정
+        # window_size = 5
+        # hidden_size = 100
+        # batch_size = 100
+        # max_epoch = 10
+
+        # # 데이터 읽기
+        # corpus, word_to_id, id_to_word = ptb.load_data('train')[:100]
+        # vocab_size = len(word_to_id)
+
+        # contexts, target = create_contexts_target(corpus, window_size)
+
+        # # 모델 등 생성
+        # model = CBOW(vocab_size, hidden_size, window_size, corpus)
+        # # model = SkipGram(vocab_size, hidden_size, window_size, corpus)
+        # optimizer = Adam()
+        # trainer = Trainer(model, optimizer)
+
+        # # 학습 시작
+        # trainer.fit(contexts, target, max_epoch, batch_size)
+        # trainer.plot()
+
+        # # 나중에 사용할 수 있도록 필요한 데이터 저장
+        # word_vecs = model.word_vecs
+        # params = {}
+        # params['word_vecs'] = word_vecs.astype(np.float16)
+        # params['word_to_id'] = word_to_id
+        # params['id_to_word'] = id_to_word
+        # pkl_file = 'cbow_params.pkl'  # or 'skipgram_params.pkl'
+        # with open(pkl_file, 'wb') as f:
+        #     pickle.dump(params, f, -1)
+
+        pkl_file = 'cbow_params.pkl'
+        # pkl_file = 'skipgram_params.pkl'
+
+        with open(pkl_file, 'rb') as f:
+            params = pickle.load(f)
+            word_vecs = params['word_vecs']
+            word_to_id = params['word_to_id']
+            id_to_word = params['id_to_word']
+
+        # 가장 비슷한(most similar) 단어 뽑기
+        querys = ['you', 'year', 'car', 'toyota']
+        # for query in querys:
+        #     most_similar(query, word_to_id, id_to_word, word_vecs, top=5)
+
+        col2_1, col2_2 = st.columns([1,1])
+
+        with col2_1:
+            most_similar('you', word_to_id, id_to_word, word_vecs, top=5)
+            st.write('---')
+            most_similar('year', word_to_id, id_to_word, word_vecs, top=5)
+            st.write('---')
+        
+        with col2_2:
+            most_similar('car', word_to_id, id_to_word, word_vecs, top=5)
+            st.write('---')
+            most_similar('toyota', word_to_id, id_to_word, word_vecs, top=5)
+            st.write('---')
+
+    with col1:
+        # 유추(analogy) 작업
+        analogy('king', 'man', 'queen',  word_to_id, id_to_word, word_vecs)
+        analogy('take', 'took', 'go',  word_to_id, id_to_word, word_vecs)
+        analogy('car', 'cars', 'child',  word_to_id, id_to_word, word_vecs)
+        analogy('good', 'better', 'bad',  word_to_id, id_to_word, word_vecs)
