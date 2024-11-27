15648449bc4e0a54477dbe9486fa5ef98932e3e4
insu <qkrdlstn9701@naver.com>
2024-11-28 01:08:58 +0900

    plz

--- Summary/main.py
+++ Summary/main.py
@@ -1,60 +1,94 @@
-import time
-import os
 import streamlit as st
-# seleniumbase
+from bs4 import BeautifulSoup
 from selenium import webdriver
-from seleniumbase import Driver
-from selenium.webdriver.chrome.options import Options
+from urllib.request import Request, urlopen
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
-
-def install_dependencies():
-    st.write("시스템 의존성 설치 중...")
-    os.system("sudo apt-get update")
-    os.system("sudo apt-get install -y libglib2.0-0 libnss3 libgconf-2-4")
-    st.write("설치 완료!")
-
-if st.button("의존성 설치"):
-    try:
-        install_dependencies()
-    except Exception as e:
-        st.error(f"설치 중 오류 발생: {e}")
-
-
-# Chrome 설정
-def get_chrome_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-
-    # webdriver-manager 경로를 명시적으로 설정
-    chromedriver_path = "/home/appuser/.wdm/drivers/chromedriver/linux64/114.0.5735.90/chromedriver"
-    if not os.path.exists(chromedriver_path):
-        raise FileNotFoundError(f"Chromedriver 경로를 찾을 수 없습니다: {chromedriver_path}")
-
-    service = Service(chromedriver_path)
-    return webdriver.Chrome(service=service, options=chrome_options)
-
-
-url = "https://sports.chosun.com/football/?action=worldfootball"
+# streamlit 설정
+st.set_page_config(layout="wide")
 
 # streamlit
 st.title('해외축구 뉴스 요약')
 
 key_word = st.text_input("검색하고 싶은 키워드를 입력하세요!")
-view_count = st.text_input("보고싶은 기사의 수를 입력하세요!", 1)
 
-if st.button("검색 시작"):
-    if not key_word:
-        st.error("키워드를 입력해주세요!!")
+view_count = st.text_input("보고싶은 기사의 수를 입력하세요!")
+
+if st.button("검색 시작하기"):
+    # 크롤링 시작
+    url = 'https://sports.chosun.com/football/?action=worldfootball'
+    options = webdriver.ChromeOptions()
+    # Chrome 옵션 설정
+    options.add_argument("--headless")  # 헤드리스 모드
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")  # Docker 환경에서 유용
+
+    # ChromeDriver 실행
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.maximize_window()
+    driver.get(url)
+    driver.implicitly_wait(20)
+
+    driver.find_element(By.XPATH, '/html/body/div[2]/nav/div[2]/ul/li[3]/button/i').click()
+    driver.implicitly_wait(20)
+
+    if key_word:
+        driver.find_element(By.XPATH, '//*[@id="keyword"]').send_keys(key_word)
+        driver.find_element(By.XPATH, '//*[@id="sfrm"]/button/i').click()
+        driver.implicitly_wait(20)
     else:
-        driver = get_chrome_driver()
-        driver.get(url)
-        driver.quit()
+        st.write("아직 검색어를 입력하지 않았네요.. 검색어를 입력하신 뒤 다시 클릭해주세요!!")
+        raise NotImplemented
 
-st.write("완료!")
+    if view_count:
+        view_count = int(view_count)
+        for i in range(1, view_count + 1):
+            if i == 1:
+                driver.find_element(By.XPATH,
+                                    '/html/body/div[4]/div[1]/div/div[2]/div[1]/div[2]/div/div/div/a').send_keys(
+                    Keys.CONTROL + Keys.ENTER)
+                driver.implicitly_wait(20)
+            else:
+                driver.find_element(By.XPATH,
+                                    f'/html/body/div[4]/div[1]/div/div[2]/div[1]/div[2]/div[{i}]/div/div/a').send_keys(
+                    Keys.CONTROL + Keys.ENTER)
+                driver.implicitly_wait(20)
+
+            driver.switch_to.window(driver.window_handles[-1])
+            info_url = driver.current_url  # 현재 URL
+            info_req = Request(info_url, headers={'User-Agent': 'Mozilla/5.0'})
+            info_html = urlopen(info_req).read()
+            bs_obj = BeautifulSoup(info_html, 'html.parser')
+
+            title = bs_obj.find('h1', {'class': 'article-title'}).text
+            article = bs_obj.find('div', {'class': 'article-Box'}).text
+
+            parser = PlaintextParser.from_string(article, Tokenizer("korean"))
+            summarizer = LsaSummarizer()
+            summary = summarizer(parser.document, 3)  # 요약할 문장 개수
+
+            st.write("Title : ", title)
+            st.write("URL : ", info_url)
+            for sentence in summary:
+                st.write(sentence)
+
+            st.markdown("---")
+
+            driver.close()
+            driver.switch_to.window(driver.window_handles[-1])
+    else:
+        st.write("보고싶은 기사의 수를 작성하시고 다시 눌러주세요!")
+        raise NotImplemented
+
+    driver.quit()
