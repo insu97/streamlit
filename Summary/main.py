d7b78b390a9ef729cababdec741a70fab0c25a28
insu <qkrdlstn9701@naver.com>
2024-11-27 19:06:24 +0900

    Update main.py

--- Summary/main.py
+++ Summary/main.py
@@ -34,7 +34,7 @@ if st.button("검색 시작하기"):
 
         # ChromeDriver 실행
         from selenium.webdriver.chrome.service import Service as ChromeService
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
         driver.maximize_window()
         driver.get(url)
 
