b1af8163df587363ffc1fb6799ea3043b4acec8d
insu <qkrdlstn9701@naver.com>
2024-11-28 00:35:04 +0900

    Update main.py

--- Summary/main.py
+++ Summary/main.py
@@ -16,12 +16,21 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 def get_driver():
     try:
-        return webdriver.Chrome()
-    except:
-        firefoxOptions = Options()
-        firefoxOptions.add_argument("--headless")
-        service = Service(GeckoDriverManager().install())
-        return webdriver.Firefox(options=firefoxOptions, service=service)
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+        service = Service(ChromeDriverManager().install())
+        return webdriver.Chrome(service=service, options=chrome_options)
+    except Exception as e:
+        st.write(f"Chrome initialization failed: {e}")
+        try:
+            firefoxOptions = Options()
+            firefoxOptions.add_argument("--headless")
+            service = Service(GeckoDriverManager().install())
+            return webdriver.Firefox(options=firefoxOptions, service=service)
+        except Exception as e:
+            print(f"Firefox initialization failed: {e}")
+            st.error("Unable to initialize a browser driver.")
+            return None
 
 
 url = "https://sports.chosun.com/football/?action=worldfootball"
