ead5e0715584ddf7f0f4c719ea4dcfae4b9de953
insu <qkrdlstn9701@naver.com>
2024-11-27 18:33:41 +0900

    update

--- Summary/main.py
+++ Summary/main.py
@@ -11,6 +11,21 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.core.os_manager import ChromeType
+
+@st.cache_resource
+def get_driver():
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
+
+options = Options()
+options.add_argument("--disable-gpu")
+options.add_argument("--headless")
 
 # streamlit 설정
 st.set_page_config(layout="wide")
@@ -29,15 +44,9 @@ if st.button("검색 시작하기"):
     else:
         # 크롤링 시작
         url = 'https://sports.chosun.com/football/?action=worldfootball'
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")  # 헤드리스 모드
-        options.add_argument("--disable-gpu")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")  # Docker 환경에서 유용
 
         # ChromeDriver 실행
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=options)
+        driver = get_driver()
         driver.maximize_window()
         driver.get(url)
 
