bcc18e9ed29aaacf2b67b4469f71a528e2430b75
insu <qkrdlstn9701@naver.com>
2024-11-28 10:28:43 +0900

    Update main.py

--- Summary/main.py
+++ Summary/main.py
@@ -1,18 +1,26 @@
 import streamlit as st
-import os, sys
-
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-
-_ = installff()
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-# opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+
+@st.cache_resource
+def get_driver():
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
 
 url = 'https://sports.chosun.com/football/?action=worldfootball'
 
-browser.get(url)
-st.write(browser.page_source)
+options = Options()
+options.add_argument("--disable-gpu")
+options.add_argument("--headless")
+
+driver = get_driver()
+driver.get(url)
+
+st.code(driver.page_source)
