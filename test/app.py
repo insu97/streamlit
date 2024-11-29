ca4685dffd655ba6118926aaea1b10cc5384646a
insu <qkrdlstn9701@naver.com>
2024-11-29 23:55:15 +0900

    Update app.py

--- test/app.py
+++ test/app.py
@@ -9,7 +9,10 @@ from selenium.webdriver.support import expected_conditions as EC
 @st.cache_resource
 def get_driver():
     options = Options()
-    options.add_argument('-headless')
+    options.add_argument('--headless=new')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
     return webdriver.Firefox(options=options)
 
 
